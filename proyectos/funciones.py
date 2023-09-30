403de8a3cd7f54168518adc8797559662a7bb815
DI-LOM <octavio.velo2022@gmail.com>
2023-09-30 16:54:16 -0300

    Update funciones.py

--- proyectos/funciones.py
+++ proyectos/funciones.py
@@ -34,15 +34,7 @@ def op1(v):
                     v.append(t)
                 vuelta += 1
         else:
-            # lo que hace es si ya existen arreglos creados los deja como esta y agrega desde los arreglos desde el texto.
-            # dejando a los creados primero y a los del texto despues
-            m = open("peajes-tp3.txt", "rt")
-            vuelta = len(v)
-            for i in m:
-                if vuelta >= (len(v) + 1):
-                    t = Ticket(i[0:10], i[10:17], i[17], i[18], i[19], i[20:23])
-                    v.append(t)
-                vuelta += 1
+            pass
     return v
 
 
